f09237c30516a859614e0af7980dc3a1f738df11
Trung Nguyen Quang <trungnq@avadagroup.com>
2026-03-11 16:51:29 +0700

    Update default api url

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const DEFAULT_API_URL = 'https://avada-crm-local.firebaseapp.com/crm/api/v1/firestore-sync';
+export const DEFAULT_API_URL = 'https://avada-crm.firebaseapp.com/crm/api/v1/firestore-sync';
 export const DEFAULT_TIMEOUT = 30000;
